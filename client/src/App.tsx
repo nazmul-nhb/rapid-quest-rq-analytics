422b007461fc2ca5419049bc4a7a1724a52587f0
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-08-25 03:19:35 +0600

    updated site title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import CLTVReport from "./components/CLTVReport";
 const App = () => {
 	return (
 		<main className="px-8 mx-auto w-full">
+			<h3 className="text-center">Rapid Quest Analytics</h3>
 			<CLTVReport/>
 		</main>
 	);
